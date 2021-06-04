ffa4f231b20461568b822cfe3e68b2289bfffe1d
khalid-davis <qinkai1994@qq.com>
2021-06-04 11:48:16 +0800

    add relay

---
+++ little_project/proxy/proxy/relay-libp2p/happy/relay2.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/libp2p/go-libp2p"
+	circuit "github.com/libp2p/go-libp2p-circuit"
+	"khalid.foundation/proxy/proxy/relay-libp2p/utils"
+)
+
+func main() {
+	listenPort := 10002
+	host, err := libp2p.New(
+		context.Background(),
+		libp2p.ListenAddrStrings(fmt.Sprintf("/ip4/0.0.0.0/tcp/%d", listenPort)),
+		libp2p.EnableRelay(circuit.OptHop),
+		libp2p.ForceReachabilityPrivate(),
+	)
+	if err != nil {
+		log.Printf("Failed to create relay-libp2p server: %s", err)
+		return
+	}
+
+	utils.PrintHostAddr("RELAY 2: ", host)
+
+	select {}
+
+}
