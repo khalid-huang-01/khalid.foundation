505b5540de4b8b68f21ae1bc96bcec9bef10b47e
khalid-davis <qinkai1994@qq.com>
2021-07-26 11:50:56 +0800

    update

---
+++ little_project/proxy/examples/p2p-demo/basic-ping-host-with-mdns/msdn-same-lan/ping-client.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/libp2p/go-libp2p/p2p/protocol/ping"
+	"khalid.foundation/proxy/msdn/msdn"
+	"time"
+)
+
+func main()  {
+	ctx := context.Background()
+
+	node, err := libp2p.New(ctx, libp2p.Ping(false), libp2p.ListenAddrStrings("/ip4/192.168.0.32/tcp/10007"))
+	if err != nil {
+		panic(err)
+	}
+
+	pingService := &ping.PingService{Host: node}
+
+	// pint he node's peerInfo in multiaddr format
+	peerInfo := peer.AddrInfo{
+		ID:    node.ID(),
+		Addrs: node.Addrs(),
+	}
+	addrs, err := peer.AddrInfoToP2pAddrs(&peerInfo)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("libp2p node address: ", addrs[0])
+
+	peerChan := msdn.InitMDNS(ctx, node, "meetup-13")
+
+
+	fmt.Println("start listener")
+	for p := range peerChan {
+		fmt.Println("Found p:", p, ", connecting")
+		if err := node.Connect(ctx, p); err != nil {
+			fmt.Println("Connection failed:", err)
+			continue
+		}
+		fmt.Println("Connection success")
+		ch := pingService.Ping(ctx, p.ID)
+		go func(ch <-chan ping.Result, addrInfo peer.AddrInfo) {
+			for i := 0; i < 5; i++ {
+				res := <-ch
+				fmt.Println("pinged", addrInfo.Addrs, "in", res.RTT)
+				time.Sleep(10 * time.Second)
+			}
+		}(ch, p)
+	}
+
+	fmt.Println("exit")
+	if err := node.Close(); err != nil {
+		panic(err)
+	}
+}
