2405f92999ba761f29fa95eef44a7eb866c1abf2
khalid-davis <qinkai1994@qq.com>
2021-08-17 09:36:33 +0800

    update

---
+++ little_project/libp2p/basic-echo-project/echo-client.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/multiformats/go-multiaddr"
+	"io/ioutil"
+	"log"
+)
+
+func main()  {
+	ctx := context.Background()
+
+	// 这里使用libp2p.NoSecurity，如果对端不使用这个或者Security里面的协议不一样的化，两者就无法建立连接
+	// 因为没有对应的协议做Secrutiy
+	node, err := libp2p.New(ctx, libp2p.Ping(false), libp2p.NoSecurity)
+	if err != nil {
+		panic(err)
+	}
+
+	//if len(os.Args) <= 1 {
+	//	panic("Please provide the peer addr")
+	//}
+	//addr, err := multiaddr.NewMultiaddr(os.Args[1])
+	addrStr := "/ip4/192.168.0.10/tcp/10001/p2p/QmbSUTgoPDgRqP5S1Zz2fJJhtg8MFiQna3XAQTQRk9nDSG"
+	addr, err := multiaddr.NewMultiaddr(addrStr)
+	if err != nil {
+		panic(err)
+	}
+	peer, err := peer.AddrInfoFromP2pAddr(addr)
+	if err != nil {
+		panic(err)
+	}
+	if err := node.Connect(ctx, *peer); err != nil {
+		panic(err)
+	}
+
+	s, err := node.NewStream(ctx, peer.ID, "/echo/1.0.0")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	log.Println("Sender syaning hello")
+	_, err = s.Write([]byte("Hello, world\n"))
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	out, err := ioutil.ReadAll(s)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	log.Printf("read reply: %q\n", out)
+
+	if err := node.Close(); err != nil {
+		panic(err)
+	}}
---
+++ little_project/libp2p/basic-echo-project/echo-server.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"encoding/pem"
+	"fmt"
+	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p-core/crypto"
+	"github.com/libp2p/go-libp2p-core/network"
+	"github.com/libp2p/go-libp2p-core/peer"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+// https://docs.libp2p.io/tutorials/getting-started/go
+
+func main() {
+	// openssl genrsa -out rsa_private.key 2048
+	certBytes, err := ioutil.ReadFile("./host-acl-with-connectiongater/server.key")
+	if err != nil {
+		log.Println("unable to read client.pem, error: ", err)
+		return
+	}
+	block, _ := pem.Decode(certBytes)
+
+	priv, err := crypto.UnmarshalRsaPrivateKey(block.Bytes)
+	if err != nil {
+		panic(err)
+	}
+
+
+	ctx := context.Background()
+	// 为了更好的知道ping服务，这里禁止了直接使用内置的ping protocol
+	node, err := libp2p.New(ctx, libp2p.Ping(false),
+		libp2p.Identity(priv),
+		libp2p.ListenAddrStrings("/ip4/0.0.0.0/tcp/10001"),
+		libp2p.NoSecurity)
+
+	if err != nil {
+		panic(err)
+	}
+
+	node.SetStreamHandler("/echo/1.0.0", func(s network.Stream) {
+		log.Println("listener received new stream")
+		if err := doEcho(s); err != nil {
+			log.Println(err)
+			s.Reset()
+		} else {
+			s.Close()
+		}
+	})
+
+
+
+	// pint he node's peerInfo in multiaddr format
+	peerInfo := peer.AddrInfo{
+		ID:    node.ID(),
+		Addrs: node.Addrs(),
+	}
+	addrs, err := peer.AddrInfoToP2pAddrs(&peerInfo)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("libp2p node address: ", addrs[0])
+
+	// wait for a SIGINT or SIGTERM signal
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	<-ch
+	fmt.Println("Received signal, shutting down...")
+	if err := node.Close(); err != nil {
+		panic(err)
+	}
+
+}
+// doEcho reads a line of data a stream and writes it back
+func doEcho(s network.Stream) error {
+	buf := bufio.NewReader(s)
+	str, err := buf.ReadString('\n')
+	if err != nil {
+		return err
+	}
+
+	log.Printf("read: %s", str)
+	_, err = s.Write([]byte(str))
+	return err
+}
