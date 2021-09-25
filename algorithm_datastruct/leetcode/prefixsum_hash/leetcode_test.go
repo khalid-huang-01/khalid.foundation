1cff36e642244b23605188d8f44f9c16f9cc3648
khalid-jobs <qinkai1994@qq.com>
2021-09-25 21:09:52 +0800

    update

--- algorithm_datastruct/leetcode/prefixsum_hash/leetcode_test.go
+++ algorithm_datastruct/leetcode/prefixsum_hash/leetcode_test.go
@@ -15,3 +15,30 @@ func subarraySum(nums []int, k int) int {
 	return rsl
 }
 
+// leetcode 523
+// pre(i, j) = k * n
+// => pre(i,j) % k = (k*n) % k
+// => pre(i,j) % k = 0
+// => (pre(0,j) - pre(0, i-1)) % k = 0
+// => pre(0,j) % k = pre(0, i-1) %k
+// 条件是 j != i => 组合的数必须大于2, 而且要满足可以从小标为0开始的组合
+func checkSubarraySum(nums []int, k int) bool {
+	prefixsumMap := make(map[int]int)
+	prefixsumMap[0] = -1 // 虚拟设置下表位置-1的值为0，这样才可以从下表为0开始的地方开始
+	isExist := false
+	curSum := 0
+	for i, v := range nums {
+		curSum += v
+		tmp := curSum % k
+		_, ok := prefixsumMap[tmp]
+		if ok && prefixsumMap[tmp]+1 != i{ // 保证组合的数目必须大于2
+			isExist = true
+			break
+		}
+		if !ok {
+			prefixsumMap[tmp] = i
+		}
+	}
+	return isExist
+}
+
