124fccbc86455d8a3f8b6e09a3742474da5e451e
khalid-jobs <qinkai1994@qq.com>
2021-11-30 17:13:51 +0800

    update

--- little_project/libp2p/go.mod
+++ little_project/libp2p/go.mod
@@ -10,12 +10,16 @@ require (
 	github.com/libp2p/go-libp2p v0.13.1-0.20210224102305-f981b25d2738
 	github.com/libp2p/go-libp2p-circuit v0.4.0
 	github.com/libp2p/go-libp2p-core v0.8.5
-	github.com/libp2p/go-libp2p-discovery v0.5.0
-	github.com/libp2p/go-libp2p-kad-dht v0.11.1
+	github.com/libp2p/go-libp2p-discovery v0.5.0 // indirect
+	github.com/libp2p/go-libp2p-kad-dht v0.11.1 // indirect
 	github.com/libp2p/go-libp2p-peerstore v0.2.7 // indirect
 	github.com/libp2p/go-libp2p-swarm v0.5.0 // indirect
+	github.com/libp2p/go-libp2p-tls v0.1.3
 	github.com/libp2p/go-libp2p/examples v0.0.0-20210531210754-f0d89c2770c7 // indirect
+	github.com/libp2p/go-netroute v0.1.3
+	github.com/libp2p/go-tcp-transport v0.2.1
 	github.com/libp2p/go-yamux/v2 v2.0.0 // indirect
+	github.com/minio/sha256-simd v0.1.1
 	github.com/multiformats/go-multiaddr v0.3.1
 	github.com/prometheus/common v0.2.0 // indirect
 	github.com/wangjia184/sortedset v0.0.0-20160527075905-f5d03557ba30 // indirect
---
+++ little_project/libp2p/net/utils/ip_fetch.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"github.com/libp2p/go-netroute"
+	"net"
+)
+
+func main()  {
+	rsl := make([]net.IP, 0)
+
+	// try to use the default ipv4/6 address
+	r, err := netroute.New()
+	if err != nil {
+		panic(err)
+	}
+	_, _, localIPv4, err := r.Route(net.IPv4zero)
+	if err != nil {
+		panic(err)
+	}
+
+	if localIPv4.IsGlobalUnicast() {
+		rsl = append(rsl, localIPv4)
+	}
+	fmt.Println(rsl)
+
+	if _, _, localIPv6, err := r.Route(net.IPv6unspecified); err != nil {
+		fmt.Println(err)
+	} else if localIPv6.IsGlobalUnicast() {
+		rsl = append(rsl, localIPv6)
+	}
+
+	// resolve the interface addresses
+	if addrs, err := net.InterfaceAddrs(); err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(addrs)
+		for _, addr := range addrs {
+			ip := net.ParseIP(addr.String())
+			if !isIp6LinkLocal(ip) && ip.IsLoopback() {
+				rsl = append(rsl, ip)
+			}
+		}
+	}
+
+	for _, ip := range rsl {
+		fmt.Println(ip.String())
+	}
+
+}
+
+func isIp6LinkLocal(ip net.IP) bool {
+	return ip.IsLinkLocalMulticast() || ip.IsLinkLocalUnicast()
+}
+
